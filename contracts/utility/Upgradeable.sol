867574272602d429c08b33ed6134a0774414cfce
Ivan Zhelyazkov <ivan.y.zhelyazkov@gmail.com>
2023-03-20 12:43:46 +0200

    change license to MIT

--- contracts/utility/Upgradeable.sol
+++ contracts/utility/Upgradeable.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
 import { AccessControlEnumerableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
