d2a0babc05fdb4b734023cabc9c8ba97f8cd58e1
Ivan Zhelyazkov <ivan.y.zhelyazkov@gmail.com>
2023-05-10 10:35:02 +0300

    v4 deployment scripts

---
+++ deploy/scripts/0005-bancor-arbitrage-upgrade.ts
@@ -0,0 +1,70 @@
+import {
+    DeployedContracts,
+    InstanceName,
+    isMainnet,
+    setDeploymentMetadata,
+    upgradeProxy
+} from '../../utils/Deploy';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { BancorArbitrage } from '../../typechain-types';
+
+const func: DeployFunction = async ({ getNamedAccounts }: HardhatRuntimeEnvironment) => {
+    const {
+        deployer,
+        uniswapV2Router02,
+        uniswapV3Router,
+        sushiSwapRouter,
+        bnt,
+        dustWallet,
+        bancorNetworkV2,
+        bancorNetworkV3,
+        carbonController
+    } = await getNamedAccounts();
+
+    const exchanges: BancorArbitrage.ExchangesStruct = {
+        bancorNetworkV2,
+        bancorNetworkV3,
+        uniV2Router: uniswapV2Router02,
+        uniV3Router: uniswapV3Router,
+        sushiswapRouter: sushiSwapRouter,
+        carbonController
+    };
+
+    if (isMainnet()) {
+        await upgradeProxy(
+            {
+                name: InstanceName.BancorArbitrage,
+                from: deployer,
+                args: [bnt, dustWallet, exchanges]
+            },
+            true
+        );
+    } else {
+        const mockExchanges = await DeployedContracts.MockExchanges.deployed();
+
+        await upgradeProxy(
+            {
+                name: InstanceName.BancorArbitrage,
+                from: deployer,
+                args: [
+                    bnt,
+                    dustWallet,
+                    {
+                        bancorNetworkV2: mockExchanges.address,
+                        bancorNetworkV3,
+                        uniV2Router: mockExchanges.address,
+                        uniV3Router: mockExchanges.address,
+                        sushiswapRouter: mockExchanges.address,
+                        carbonController: mockExchanges.address
+                    }
+                ]
+            },
+            true
+        );
+    }
+
+    return true;
+};
+
+export default setDeploymentMetadata(__filename, func);
---
+++ deploy/tests/0005-bancor-arbitrage-upgrade.ts
@@ -0,0 +1,33 @@
+import { MIN_BNT_BURN } from '../../utils/Constants';
+import { BancorArbitrage, ProxyAdmin } from '../../components/Contracts';
+import { DeployedContracts, describeDeployment } from '../../utils/Deploy';
+import { toPPM, toWei } from '../../utils/Types';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describeDeployment(__filename, () => {
+    let proxyAdmin: ProxyAdmin;
+    let bancorArbitrage: BancorArbitrage;
+
+    beforeEach(async () => {
+        proxyAdmin = await DeployedContracts.ProxyAdmin.deployed();
+        bancorArbitrage = await DeployedContracts.BancorArbitrage.deployed();
+    });
+
+    it('should upgrade correctly', async () => {
+        expect(await proxyAdmin.getProxyAdmin(bancorArbitrage.address)).to.equal(proxyAdmin.address);
+        expect(await bancorArbitrage.version()).to.equal(4);
+        const implementationAddress = await proxyAdmin.getProxyImplementation(bancorArbitrage.address);
+        const bancorArbitrageImplementation: BancorArbitrage = await ethers.getContractAt(
+            'BancorArbitrage',
+            implementationAddress
+        );
+
+        const arbRewards = await bancorArbitrage.rewards();
+        expect(arbRewards.percentagePPM).to.equal(toPPM(50));
+        expect(arbRewards.maxAmount.toString()).to.equal(toWei(100).toString());
+
+        // test implementation has been initialized
+        await expect(bancorArbitrageImplementation.initialize()).to.be.rejectedWith('execution reverted');
+    });
+});
